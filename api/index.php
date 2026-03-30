cf1f2a3af5dbf737242884e924677d3c4ea13c33
Mathias Muntz <mm@novel-t.ch>
2026-03-30 13:51:50 +0200

    add paginated areas endpoint

--- api/index.php
+++ api/index.php
@@ -2,12 +2,14 @@
 
 require_once __DIR__ . '/vendor/autoload_runtime.php';
 
+use Faker\Factory;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Symfony\Component\Uid\Ulid;
 
 class Kernel extends BaseKernel
 {
@@ -23,6 +25,48 @@ class Kernel extends BaseKernel
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
         $routes->import(static::class, 'attribute');
+    }
+
+    #[Route('/api/data/areas', name: 'api_data_areas')]
+    public function dataAreas(\Symfony\Component\HttpFoundation\Request $request): JsonResponse
+    {
+        $total     = 5000;
+        $perPage   = 1000;
+        $totalPages = (int) ceil($total / $perPage);
+        $page      = max(1, min((int) $request->query->get('page', 1), $totalPages));
+        $offset    = ($page - 1) * $perPage;
+
+        $faker = Factory::create('fr_FR');
+        $faker->seed(42);
+
+        $nameCounts = [];
+        $areas = [];
+
+        for ($i = 0; $i < $offset + $perPage; $i++) {
+            $base = $faker->city();
+            if (!isset($nameCounts[$base])) {
+                $nameCounts[$base] = 0;
+                $name = $base;
+            } else {
+                $nameCounts[$base]++;
+                $name = $base . ' ' . $nameCounts[$base];
+            }
+            if ($i >= $offset) {
+                $areas[] = ['id' => (string) new Ulid(), 'name' => $name];
+            }
+        }
+
+        return new JsonResponse([
+            'data' => $areas,
+            'meta' => [
+                'total'      => $total,
+                'perPage'    => $perPage,
+                'totalPages' => $totalPages,
+                'page'       => $page,
+                'hasNext'    => $page < $totalPages,
+                'hasPrev'    => $page > 1,
+            ],
+        ]);
     }
 
     #[Route('/api/config', name: 'api_config')]
